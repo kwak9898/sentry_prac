c12a655f88a33b8d5fe01ccdf8a960944908be5b
kwak9898 <kwak9898@gitbub.com>
2022-12-14 17:33:45 +0900

    sentry setting

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import Sentry from '@sentry/node';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,6 +10,30 @@ const port = process.env.PORT;
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: 'test',
+  name: 'My First Test Transaction',
+});
+
+function foo() {
+  console.error('error');
+}
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (err) {
+    Sentry.captureException(err);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
 
 app.listen(port, () => {
   console.log(`🚀Started Server with http://localhost:${port}`);
